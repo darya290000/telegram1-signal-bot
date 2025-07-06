f6a73aa973229ea1c81ec8259a961620fb089daf
darya290000 <darya290000@gmail.com>
2025-07-06 09:28:59 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Telegram Signal Bot
-
 Simple Python bot to send a test message to Telegram using Railway environment variables.
-
-## How to use:
-- Set `TOKEN` and `CHAT_ID` in Railway environment.
-- On deploy, the bot sends a test message to your Telegram.
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import os
 import requests
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "8136421090:AAFrb8RI6BQ2tH49YXX_5S32_W0yWfT04Cg"
+CHAT_ID = "570096331"
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": text
-    }
+    payload = {"chat_id": CHAT_ID, "text": text}
     try:
         res = requests.post(url, json=payload)
         print("✅ پیام ارسال شد:", res.text)
@@ -17,4 +13,4 @@ def send_message(text):
         print("❌ خطا در ارسال پیام:", e)
 
 if __name__ == "__main__":
-    send_message("📡 سیگنال تست از ربات Railway 🚀")
+    send_message("🚀 تست موفق Railway")
